40b1a3262308bb911cca339adc6a4ce40e37bf4e
pestopancake <p@ulmart.in>
2019-02-18 23:57:34 +0000

    wip

--- src/states/Game.js
+++ src/states/Game.js
@@ -77,17 +77,17 @@ export default class extends Phaser.State {
     // player hitboxes
     this.hitboxes = this.game.add.group()
     this.hitboxes.enableBody = true
-    this.player.addChild(this.hitboxes);
-    var hbSize = this.player.width / 4;
-    this.hitboxL = this.hitboxes.create(0, 0, null);
-    this.hitboxL.name = "hitboxL";
-    this.hitboxL.body.setSize(hbSize, hbSize, -this.player.width / 2 - (hbSize / 2), -hbSize / 2);
-    this.hitboxR = this.hitboxes.create(0, 0, null);
-    this.hitboxR.name = "hitboxR";
-    this.hitboxR.body.setSize(hbSize, hbSize, (this.player.width / 2) - (hbSize / 2), -hbSize / 2);
-    this.hitboxB = this.hitboxes.create(0, 0, null);
-    this.hitboxB.name = "hitboxB";
-    this.hitboxB.body.setSize(hbSize, hbSize, -hbSize / 2, (this.player.height / 2) - (hbSize / 2));
+    this.player.addChild(this.hitboxes)
+    var hbSize = this.player.width / 4
+    this.hitboxL = this.hitboxes.create(0, 0, null)
+    this.hitboxL.name = 'hitboxL'
+    this.hitboxL.body.setSize(hbSize, hbSize, -this.player.width / 2 - (hbSize / 2), -hbSize / 2)
+    this.hitboxR = this.hitboxes.create(0, 0, null)
+    this.hitboxR.name = 'hitboxR'
+    this.hitboxR.body.setSize(hbSize, hbSize, (this.player.width / 2) - (hbSize / 2), -hbSize / 2)
+    this.hitboxB = this.hitboxes.create(0, 0, null)
+    this.hitboxB.name = 'hitboxB'
+    this.hitboxB.body.setSize(hbSize, hbSize, -hbSize / 2, (this.player.height / 2) - (hbSize / 2))
 
     this.generateBlocks()
 
@@ -96,12 +96,14 @@ export default class extends Phaser.State {
     this.digButton = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN)
     this.actionButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
-    this.actionButton.onDown.add(function(){
-      if(this.activeBlock){
-        this.seedBlock = this.activeBlock;
+    this.actionButton.onDown.add(function () {
+      if (this.activeBlock) {
+        this.activeBlock.genetics = {
+          lifespan: 5
+        }
+        this.seedBlock = this.activeBlock
       }
-    }, this);
-
+    }, this)
   }
 
   render () {
@@ -110,20 +112,10 @@ export default class extends Phaser.State {
     }
   }
 
-
-
-
-
-
-
-
-
-
-
-  reset(a){
-    this.player.reset(350,100);
-    this.blocks.destroy();
-    this.generateBlocks();
+  reset (a) {
+    this.player.reset(350, 100)
+    this.blocks.destroy()
+    this.generateBlocks()
   }
 
   update () {
@@ -137,31 +129,28 @@ export default class extends Phaser.State {
 
     this.game.physics.arcade.overlap(this.hitboxes, this.blocks, function (a,b) {
       // destroyblockRallowed = false;
-      if (a.name == 'hitboxL') {
+      if (a.name === 'hitboxL') {
         this.activeBlockl = b
         this.activeBlockl.alpha = 0.8
-      }
-      else if(a.name == 'hitboxR') {
+      } else if (a.name === 'hitboxR') {
         this.activeBlockr = b
         this.activeBlockr.alpha = 0.8
+      } else if (a.name === 'hitboxB') {
+        this.activeBlock = b
+        this.activeBlock.alpha = 0.8
       }
-      else if(a.name == 'hitboxB'){
-        this.activeBlock = b;
-        this.activeBlock.alpha = 0.8;
-      }
-    }, null, this);
+    }, null, this)
 
-    this.game.physics.arcade.collide(this.player, this.blocks, function(a,b){
-    }, null, this);
+    this.game.physics.arcade.collide(this.player, this.blocks, function (a, b) {
+    }, null, this)
 
-
-    if(this.player.body.touching.down){
+    if (this.player.body.touching.down) {
       // game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-      this.game.camera.target = this.player;
-    }else{
-      var b = this.blocks.getFirstAlive();
+      this.game.camera.target = this.player
+    } else {
+      var b = this.blocks.getFirstAlive()
       // game.camera.follow(b, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-      this.game.camera.target = null;
+      this.game.camera.target = null
     }
 
     if (this.player.body.touching.down) {
@@ -178,184 +167,176 @@ export default class extends Phaser.State {
     } else {
       if (this.activeBlockrTimer) this.game.time.events.remove(this.activeBlockrTimer)
       if (this.activeBlocklTimer) this.game.time.events.remove(this.activeBlocklTimer)
-      this.activeBlockrWindup = false;
-      this.activeBlocklWindup = false;
-      this.destroyblockRallowed = false;
-      this.destroyblockLallowed = false;
+      this.activeBlockrWindup = false
+      this.activeBlocklWindup = false
+      this.destroyblockRallowed = false
+      this.destroyblockLallowed = false
     }
 
-    if (!this.cursors.left.isDown && !this.cursors.right.isDown){
-        if(this.player.body.velocity.x > 0 || this.player.body.velocity.x < 0){
-            if(this.player.body.velocity.x < 0.1 && this.player.body.velocity.x > -0.1){
-              this.player.body.velocity.x = 0;
-            }
-            else{
-              this.player.body.velocity.x = this.player.body.velocity.x * 0.9;
-            }
+    if (!this.cursors.left.isDown && !this.cursors.right.isDown) {
+      if (this.player.body.velocity.x > 0 || this.player.body.velocity.x < 0) {
+        if (this.player.body.velocity.x < 0.1 && this.player.body.velocity.x > -0.1) {
+          this.player.body.velocity.x = 0
+        } else {
+          this.player.body.velocity.x = this.player.body.velocity.x * 0.9
         }
+      }
     }
-    if(this.player.body.velocity.x !== 0
-    || this.player.body.velocity.y !== 0){
-      this.player.animations.play('walk', 5, true);
-    }
-    else{
-      this.player.animations.stop(null, true);
+    if (this.player.body.velocity.x !== 0 || this.player.body.velocity.y !== 0) {
+      this.player.animations.play('walk', 5, true)
+    } else {
+      this.player.animations.stop(null, true)
     }
 
-    if (this.cursors.left.isDown)
-    {
-        if(!this.activeBlockl){
-            if(this.player.body.velocity.x > -250){
-              this.player.body.velocity.x -= 10;
-            }
+    if (this.cursors.left.isDown) {
+      if (!this.activeBlockl) {
+        if (this.player.body.velocity.x > -250) {
+          this.player.body.velocity.x -= 10
         }
-        else{
-          this.player.body.velocity.x = 0;
-        }
+      } else {
+        this.player.body.velocity.x = 0
+      }
 
-        if(this.player.body.onFloor() || this.player.body.touching.down){
-            if(this.activeBlockl && !this.activeBlocklWindup){
-              this.activeBlocklWindup = true;
-              this.activeBlocklTimer = this.game.time.events.loop(
-                500,
-                function(){
-                  this.activeBlocklWindup = false;
-                  this.destroyblockLallowed = true;
-                  this.game.time.events.remove(this.activeBlocklTimer);
-                },
-                this
-              );
-            }
-            if(this.activeBlockl && this.destroyblockLallowed){
-              this.destroyblockLallowed = false;
-              this.activeBlockl.destroy();
-            }
+      if (this.player.body.onFloor() || this.player.body.touching.down) {
+        if (this.activeBlockl && !this.activeBlocklWindup) {
+          this.activeBlocklWindup = true
+          this.activeBlocklTimer = this.game.time.events.loop(
+            500,
+            function () {
+              this.activeBlocklWindup = false
+              this.destroyblockLallowed = true
+              this.game.time.events.remove(this.activeBlocklTimer)
+            },
+            this
+          )
         }
-    }
-    else if (this.cursors.right.isDown)
-    {
-        if(!this.activeBlockr){
-            if(this.player.body.velocity.x < 250){
-              this.player.body.velocity.x += 10;
-            }
+        if (this.activeBlockl && this.destroyblockLallowed) {
+          this.destroyblockLallowed = false
+          this.activeBlockl.destroy()
         }
-        else{
-          this.player.body.velocity.x = 0;
+      }
+    } else if (this.cursors.right.isDown) {
+      if (!this.activeBlockr) {
+        if (this.player.body.velocity.x < 250) {
+          this.player.body.velocity.x += 10
         }
+      } else {
+        this.player.body.velocity.x = 0
+      }
 
-        if(this.player.body.onFloor() || this.player.body.touching.down){
-            if(this.activeBlockr && !this.activeBlockrWindup){
-              this.activeBlockrWindup = true;
-              this.activeBlockrTimer = this.game.time.events.loop(
-                500,
-                function(){
-                  this.activeBlockrWindup = false;
-                  this.destroyblockRallowed = true;
-                  this.game.time.events.remove(this.activeBlockrTimer);
-                },
-                this
-              );
-            }
-            if(this.activeBlockr && this.destroyblockRallowed){
-              this.destroyblockRallowed = false;
-              this.activeBlockr.destroy();
-            }
+      if (this.player.body.onFloor() || this.player.body.touching.down) {
+        if (this.activeBlockr && !this.activeBlockrWindup) {
+          this.activeBlockrWindup = true
+          this.activeBlockrTimer = this.game.time.events.loop(
+            500,
+            function () {
+              this.activeBlockrWindup = false
+              this.destroyblockRallowed = true
+              this.game.time.events.remove(this.activeBlockrTimer)
+            },
+            this
+          )
         }
-
+        if (this.activeBlockr && this.destroyblockRallowed) {
+          this.destroyblockRallowed = false
+          this.activeBlockr.destroy()
+        }
+      }
     }
 
-    if(this.beginJump && !this.jumpButton.isDown){
-      this.beginJump = false;
+    if (this.beginJump && !this.jumpButton.isDown) {
+      this.beginJump = false
     }
-    if (this.jumpButton.isDown && (this.player.body.onFloor() || this.player.body.touching.down || this.beginJump))
-    {
-        if(this.beginJump){
-          this.player.body.velocity.y -= 50 / (this.beginJump*0.4);
-            if(this.beginJump > 200){
-              this.beginJump = false;
-            }
-            ++this.beginJump;
-        }else{
-          this.player.body.velocity.y = -150;
-          this.beginJump = 1;
+    if (this.jumpButton.isDown && (this.player.body.onFloor() || this.player.body.touching.down || this.beginJump)) {
+      if (this.beginJump) {
+        this.player.body.velocity.y -= 50 / (this.beginJump * 0.4)
+        if (this.beginJump > 200) {
+          this.beginJump = false
         }
+        ++this.beginJump
+      } else {
+        this.player.body.velocity.y = -150
+        this.beginJump = 1
+      }
     }
 
-    if (this.digButton.isDown && (this.player.body.onFloor() || this.player.body.touching.down)
-      && this.activeBlock
-      && !this.blockDestroyCooldown)
-    {
-      this.blockDestroyCooldown = true;
-      this.activeBlock.destroy();
+    if (
+      this.digButton.isDown &&
+      (
+        this.player.body.onFloor() || 
+        this.player.body.touching.down
+      ) &&
+      this.activeBlock &&
+      !this.blockDestroyCooldown
+    ) {
+      this.blockDestroyCooldown = true
+      this.activeBlock.destroy()
       this.blockDestroyCooldownTimer = this.game.time.events.loop(
-          150,
-          function(){
-            this.blockDestroyCooldown = false;
-            this.game.time.events.remove(this.blockDestroyCooldownTimer);
-          },
-          this
-        );
+        150,
+        function () {
+          this.blockDestroyCooldown = false
+          this.game.time.events.remove(this.blockDestroyCooldownTimer)
+        },
+        this
+      )
     }
   }
 
-
   blockAt (x, y) {
-    var spaceTaken = false;
-    this.blocks.forEach(function(b) {
-      if(b.x == x
-        && b.y == y){
-          spaceTaken = true;
+    var spaceTaken = false
+    this.blocks.forEach(function (b) {
+      if (b.x === x && b.y === y) {
+        spaceTaken = true
       }
-    });
-    return spaceTaken;
+    })
+    return spaceTaken
   }
 
   generateBlocks () {
-    this.blocks = this.game.add.physicsGroup();
+    this.blocks = this.game.add.physicsGroup()
 
     var height = 8
-    var width = 8;
-    var blockSize = 32;
+    var width = 8
+    var blockSize = 32
     var startX = (this.game.width / 2) - ((width * blockSize) / 2);
-    var startY = (this.game.width / 2) - ((width * blockSize) / 2);
+    var startY = (this.game.width / 2) - ((width * blockSize) / 2)
 
-    for(var x = 1; x<width; ++x){
-      for(var y = 1; y<height; ++y){
+    for (var x = 1; x < width; ++x) {
+      for (var y = 1; y < height; ++y) {
         var block = this.blocks.create(
-          startX + (x*blockSize),
-          startY + (y*blockSize),
+          startX + (x * blockSize),
+          startY + (y * blockSize),
           'block'
-        );
-        block.tint = Math.random() * 0xffffff;
+        )
+        block.tint = Math.random() * 0xffffff
       }
     }
-    this.blocks.setAll('body.immovable', true);
+    this.blocks.setAll('body.immovable', true)
 
-    //seed test
+    // seed test
     this.newBlockTestTimer = this.game.time.events.loop(
       200,
-      function(){
-        if(this.seedBlock && this.seedBlock.alive){
-          var spaceTaken = this.blockAt(this.seedBlock.x + (blockSize), this.seedBlock.y);
-          if(!spaceTaken){
-            var block = this.blocks.create(
-              this.seedBlock.x + (blockSize),
-              this.seedBlock.y,
-              'block'
-            );
-            block.body.immovable = true;
-            block.tint = Math.random() * 0xffffff;
-            this.seedBlock = block;
+      function () {
+        if (this.seedBlock && this.seedBlock.alive) {
+          if (this.seedBlock.genetics.lifespan > 0) {
+            var spaceTaken = this.blockAt(this.seedBlock.x + (blockSize), this.seedBlock.y)
+            if (!spaceTaken) {
+              var block = this.blocks.create(
+                this.seedBlock.x + (blockSize),
+                this.seedBlock.y,
+                'block'
+              )
+              block.body.immovable = true
+              block.tint = Math.random() * 0xffffff
+              block.genetics = {
+                lifespan: this.seedBlock.genetics.lifespan - 1
+              }
+              this.seedBlock = block
+            }
           }
         }
-
-        //game.time.events.remove(blockDestroyCooldownTimer);
       },
       this
-    );
-}
-
-
-
-
+    )
+  }
 }
